8cc1c4f63a28c3aa3a7a8500a51d2986fe631cbe
Yair Charit <yaircharit@gmail.com>
2021-12-20 19:28:15 +0200

    Cellular Automaton Analaysis Class is completed

--- Q2.py
+++ Q2.py
@@ -2,17 +2,16 @@ from numpy.ma.core import array
 import seagull as sg
 from seagull.lifeforms import Custom
 from seagull.utils.statistics import cell_coverage
-import random
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.random as rand
-
+from matplotlib import animation
 
 
 # board = sg.Board(size=(__board_size,__board_size))
 
 def create_random_life():
-        res = rand.randint(2,size=(CellAuto.starter_size,CellAuto.starter_size))
+        res = rand.randint(2,size=(RandomCellularAutomatonAnalyzer.starter_size,RandomCellularAutomatonAnalyzer.starter_size))
         return res
 
 def compare_matrices(a,b):
@@ -24,72 +23,81 @@ def compare_matrices(a,b):
     # print('\nxor', xor)
     # print('any xor:',xor.any(),'all xor:',xor.all())
     return not xor.any()
-class CellAuto:
+class RandomCellularAutomatonAnalyzer:
     __count = 0
-    board_size =10
+    board_size =400
     starter_size = 5
     iterations_number = 100
-    iterations_in_batch = 5
+    
     def __init__(self):
-        self.__starter = create_random_life()
-        self.__size = sum([sum (line) for line in self.__starter])
-        self.__time = 1
-        self.__status = 'alive' if self.__size > 0 else 'dead'  # alive, dead, looper
-        self.__fitness_list = list()
-        self.board = sg.Board((CellAuto.board_size,CellAuto.board_size))
-        self.board.add(Custom(self.__starter), loc=(CellAuto.board_size//2,CellAuto.board_size//2))
-        self.hist = list(self.board.state)
-        self.__coverage = self.__size / CellAuto.board_size
-        CellAuto.__count += 1
-        self.__id = CellAuto.__count
+        self.starter = create_random_life()
+        self.board = sg.Board((RandomCellularAutomatonAnalyzer.board_size,RandomCellularAutomatonAnalyzer.board_size))
+        self.board.add(Custom(self.starter),loc=((RandomCellularAutomatonAnalyzer.board_size//2,RandomCellularAutomatonAnalyzer.board_size//2)))
         self.sim = sg.Simulator(self.board)
-        self.__fitness_list.append(self.__fitness__())
-        # You can also get it using get_history()
-        # self.hist = self.sim.get_history()
-        # self.stats = self.sim.compute_statistics(self.hist)
-        # self.__f = self.fitness()
+        self.current = self.board.state
+        self.hist = []
+        self.coverage_list = []
+        self.status = 'alive'
+        self.time = 0
+        RandomCellularAutomatonAnalyzer.__count += 1
+        self.id = RandomCellularAutomatonAnalyzer.__count
+        
+    def run(self, iters=iterations_number):
+        count_down = 10
+        for i in range(iters): 
+            if self.status == 'alive':
+                self.time += 1
+                self.coverage_list.append(cell_coverage(self.current))
+                self.hist.append(self.current)
+                if any((compare_matrices(self.current, b) for b in self.hist[self.time-400:])):
+                    self.status = 'a looper'
+                else:
+                    self.status = 'alive' if self.coverage_list[-1] > 0 else 'dead'  # alive, dead, looper     
+            else:
+                count_down -= 1     
+                if count_down == 0:
+                    break
+            self.current = sg.rules.conway_classic(self.current)
+        self.peak_coverage = max(self.coverage_list)    
+        print(self)
+        return self.status
         
     def fitness(self):
-        res = sum(self.__fitness_list)/self.__time
+        res = sum(self.coverage_list)*self.time/RandomCellularAutomatonAnalyzer.iterations_number
         return res
+
     
-    def __fitness__(self):
-        return self.__coverage * self.__time
-    
-    def run(self):
-        for i in range(CellAuto.iterations_number//CellAuto.iterations_in_batch):
-            self.sim.run(sg.rules.conway_classic, iters=CellAuto.iterations_in_batch)
-            self.hist = self.sim.get_history()
-            self.current = self.hist[-1]
-            self.__size = sum([sum(line) for line in self.current])
-            self.__coverage = cell_coverage(self.current)
-            if self.__status != 'looper':
-                self.__status = 'alive' if self.__coverage > 0 else 'dead'  # alive, dead, looper
-            self.__time += 1*CellAuto.iterations_in_batch
-            if self.__status != 'alive':
-                print(self)
-                break
-            print(self.__time, len(self.hist[:-1]))
-            print([compare_matrices(self.current, b) if self.current is not b else None for b in self.hist[:-1]])
-            if any((compare_matrices(self.current, b) for b in self.hist[:-1])):
-                self.__status = 'looper'
-            self.__fitness_list.append(self.__fitness__())
-        print(self)
-        anim = sg.Simulator(self.board)
-        # iters = self.__time+10 if self.__status == 'dead' else self.__time
-        anim.run(sg.rules.conway_classic, iters=self.__time+10 if self.__status == 'dead' else self.__time)
-        anim = anim.animate()
+    def animate(self):
+        fig = plt.figure(figsize=(5,5))
+        ax = fig.add_axes([0, 0, 1, 1], xticks=[], yticks=[], frameon=False)
+        X_blank = np.zeros(self.board.size, dtype=bool)
+        im = ax.imshow(X_blank, cmap=plt.cm.binary, interpolation="nearest")
+        im.set_clim(-0.05, 1)
+
+        def _animate(i, history):
+            self.current_pos = history[i]
+            im.set_data(self.current_pos)
+            return (im,)
+
+        def _init():
+            im.set_data(X_blank)
+            return (im,)
+
+        history = np.asarray(self.hist)
+        animation.FuncAnimation(
+            fig,
+            func=_animate,
+            frames=range(history.shape[0]),
+            init_func=_init,
+            interval=100 if self.time <= 100 else 50,
+            fargs=(history,),
+            blit=True,
+        )
         plt.show()
         
-        
     def __str__(self):
-        return f'\n\nCellAuto #{self.__id} is {self.__status}: f={self.__fitness__()} time={self.__time}, f_total={self.fitness()}\n\n'
-
-# class Genetics:
-#     sim 
-            
-# board.add(Custom([[0,1,1,0], [0,0,1,1]]), loc=(0,0))
-c= CellAuto()
-print(c)
-c.run()
-print(c)
+        return f'\nRandomCellularAutomatonAnalyzer #{self.id} is {self.status}: time={self.time}, f_total={self.fitness()}\n\n'
+max_iterations = 10000
+c= RandomCellularAutomatonAnalyzer()
+while c.status == 'alive' and c.time <= max_iterations: c.run()
+c.animate()
