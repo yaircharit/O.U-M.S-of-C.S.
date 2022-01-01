9757274790f68929915c17b57c70fb29b2930b5b
Yair Charit <yaircharit@gmail.com>
2022-01-01 23:31:22 +0200

    Ready to start trainin!
    PreProcess letters using font_recognition.py
    Outputs fonts_data.hdf5 dataset
    An example for use of said dataset is in NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,13 +1,12 @@
 import h5py
 
-db = h5py.File('Data.h5')
-im_names = list(db['data'].keys())
-print(im_names)
+db = h5py.File('fonts_data.hdf5')
+im_names = list(db.keys())
 
 im = im_names[0]
-img = db['data'][im][:]
-font = db['data'][im].attrs['font']
-txt = db['data'][im].attrs['txt']
-charBB = db['data'][im].attrs['charBB']
-wordBB = db['data'][im].attrs['wordBB']
-print(db['data'][im])
+img = db[im][:]
+font = db[im].attrs['font']
+# txt = db['data'][im].attrs['txt']
+# charBB = db['data'][im].attrs['charBB']
+# wordBB = db['data'][im].attrs['wordBB']
+print(img)
